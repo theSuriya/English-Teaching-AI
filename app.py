6eb8e68bbdbc5a2036892f22f1e862b3e4c11033
suriya <127676318+theSuriya@users.noreply.github.com>
2024-05-31 18:28:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import google.generativeai as genai
 import os
+from dotenv import load_dotenv
 import requests
 
+# Load environment variables
+load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 model = genai.GenerativeModel("gemini-1.5-pro-latest")
